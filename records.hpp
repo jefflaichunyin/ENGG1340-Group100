402fb12f59f741a57b14756be5405051e5762b32
Jeff Lai <jeffcyl@connect.hku.hk>
2019-03-24 17:35:14 +0800

    modified:   records.cpp 	modified:   records.hpp

--- records.hpp
+++ records.hpp
@@ -1,48 +1,21 @@
-#ifndef CTIME_H
-#define CTIME_H
-#include <ctime>
+// This class "Records" is used to manage a collection of records of an user
+#ifndef RECORD_H
+#define RECORD_H
+#include "record.hpp"
 #endif
 
-#ifndef VECTOR_H
-#define VECTOR_H
-#include <vector>
-#endif
-class Record
-{
-    public:
-        bool Record(); //constructor
-        //getters
-        time_t getDate();
-        float getAmount();
-        string getType();
-        string getAccount();
-        string getRemark();
-        //setters with data validation
-        //return true if valid
-        bool setDate(time_t date);
-        bool setAmount(float amount);
-        bool setType(string type);
-        bool setAccount(string account);
-        bool setRemark(string remark);
-    private:
-        time_t  _date;
-        float   _amount;
-        string  _type;
-        string  _account;
-        string  _remark;
-};
-
+#include <string>
 class Records
 {
     public:
         Record getRecord(int no);
         bool addRecord(Record record);
         bool deleteRecord(int no);
-        bool modifyRecord(int no, Record record);
+        bool modifyRecord(int no, Record record); //replace old record with new record
         void sortRecords(int key, bool ascending);
-        bool readRecords(string location);
-        bool writeRecords(string location);
+        bool readRecords(std::string location);
+        bool writeRecords(std::string location);
     private:
-        vector<Record> _records;
+        std::vector<Record> _records;
 };
 
