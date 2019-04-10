f28884e1ae4636f75d00d798a060dae360e00906
Jeff Lai <jeffcyl@connect.hku.hk>
2019-04-11 00:02:22 +0800

    implement all basic functions in User and UserAccounts, not yet able to save user info to disk

--- user.cpp
+++ user.cpp
@@ -1,4 +1,5 @@
 #include "user.h"
+#include <iostream>
 bool User::setUsername(std::string username)
 {
     if(_username == "")
@@ -39,4 +40,89 @@ User::User(std::string username, std::string password)
 {
     _username = username;
     _password = password;
+}
+
+// Function: return the position of the user in the _users vector
+//           return -1 if username is not found in the _users vector
+int UserAccounts::searchUser(std::string username)
+{
+    int index = -1;
+    for(index = 0; index < int(_users.size()); index++)
+    {
+        if(_users[index].getUsername() == username)
+            break;
+    }
+    return index;
+}
+bool UserAccounts::addUser(std::string username, std::string password)
+{
+    if(searchUser(username) == -1) // username is not found
+    {
+        User *newUser = new User(username, password);
+        _users.push_back(*newUser); // copy new user to vector _users
+        delete newUser;
+        std::cout << "User " << username << " is created sucessfully\n";
+        return true; // sucessfully add the user        
+    }
+    else
+    {
+        std::cout << "User " << username << " already exist! Please choose another username.\n";
+        return false;
+    }
+}
+
+bool UserAccounts::checkPassword(std::string username, std::string password)
+{
+    int index = searchUser(username);
+    if(index == -1) // user doesn't exist
+    {
+        std::cout << username << " doesn't exist. Please try again.\n";
+        return false;
+    }
+    else if(_users[index].getPassword() != password) // user exist but password is incorrect
+    {
+        std::cout << "Incorrect password. Please enter the password correctly.\n";
+        return false;
+    }
+    else
+    {
+        std::cout << "Password correct.\n";
+        return true;
+    }
+}
+
+bool UserAccounts::removeUser(std::string username, std::string password)
+{
+    if(checkPassword(username, password) == true)
+    {
+        _users.erase(_users.begin() + searchUser(username));
+        std::cout << username << " is deleted.\n";
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool UserAccounts::changePassword(std::string username, std::string old_password, std::string new_password)
+{
+    if(checkPassword(username, old_password) == true)
+    {
+        if(old_password == new_password)
+        {
+            std::cout << "New password cannot be the same as the old password. Please use a different password.\n";
+            return false;
+        }
+        else
+        {
+            _users[searchUser(username)].setPassword(new_password);
+            std::cout << "Password changed sucessfully.\n";
+            return true;
+        }
+    }
+    else
+    {
+        return false;
+    }
 }
--- user.h
+++ user.h
@@ -16,12 +16,15 @@ class User
 class UserAccounts
 {
     public:
-        // all 3 functions will return true if sucess, false will be returned if the operation if unsucessful and the reason why will be printed
-        // e.g user already exist, user doesn't exist, password incorrect, password cannot be same
+        // all 3 functions will return true if sucess, false will be returned if the operation if unsucessful and the reason why will be printed out
+        // e.g user already exist, user doesn't exist, password incorrect, password cannot be same ...
+        
         bool addUser(std::string username, std::string password);
+        bool removeUser(std::string username, std::string password);
         bool checkPassword(std::string username, std::string password);
         bool changePassword(std::string username, std::string old_password, std::string new_password); // return false if old_password doesn't match current password or user doesn't exist
     private:
+        int searchUser(std::string username);
         std::vector<User> _users;
     
 };
