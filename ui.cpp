d607fd38e7619ca6e75818b034c4bb8994c72600
kiwicheng <vchengym@hku.hk>
2019-04-18 00:35:58 +0800

    ui.cpp add user

--- ui.cpp
+++ ui.cpp
@@ -6,12 +6,14 @@ void startUI()
     cout << "start\n";
 
     UserAccounts user_accounts;
-    user_accounts.addUser("Jeff", "123");
+    user_accounts.addUser("kiwi", "234");
+    user_accounts.addUser("jeff","123");
     Record *new_record = new Record();
     new_record->setAccount("Bank");
-    user_accounts.getUser("Jeff")->getUserRecords()->addRecord(*new_record);
+    user_accounts.getUser("kiwi")->getUserRecords()->addRecord(*new_record);
     delete new_record;
-    
-    cout << user_accounts.getUser("Jeff")->getUserRecords()->getRecord(0)->getAccount();
-    
+
+
+    cout << user_accounts.getUser("kiwi")->getUserRecords()->getRecord(0)->getAccount();
+
 }
