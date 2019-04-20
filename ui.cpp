b276e121623830203d3d42e7e03eb862423241f2
kiwicheng <vchengym@hku.hk>
2019-04-20 22:53:27 +0800

    create new user account

--- ui.cpp
+++ ui.cpp
@@ -1,19 +1,35 @@
 #include "user.h"
 #include "ui.h"
+#include <iostream>
+#include <string>
 using namespace std;
-void startUI()
+int main()
 {
+    int lorc=0;
+    //UserAccounts user_accounts;
     cout << "start\n";
+    cout << "1. Log in\n2. Create account"<<endl;
+    while(lorc!=1 && lorc!= 2){
+        cin >> lorc;   //get user preference
+    //if(lorc==1){
+       // if(user_account.loadUserInfo())
+    //}
 
-    UserAccounts user_accounts;
-    user_accounts.addUser("kiwi", "234");
-    user_accounts.addUser("jeff","123");
-    Record *new_record = new Record();
-    new_record->setAccount("Bank");
-    user_accounts.getUser("kiwi")->getUserRecords()->addRecord(*new_record);
-    delete new_record;
+    if (lorc==2){
+        string *username= new string;
+        string *userpw= new string;
+        string *verification = new string; //double data entry for password
+        cout << "User name: " <<endl;
+        //fflush(stdout);
+        getline(cin,*username);
+        cout << "User password: " <<endl;
+        //maskuserpw(*userpw);
+        cin >> *userpw;
+        cout << "input user password again: " <<endl;
+       // maskuserpw(*verification);
+        cin >> *verification;
+        cout<<*username<<" "<<*userpw<<endl;
+        }
 
-
-    cout << user_accounts.getUser("kiwi")->getUserRecords()->getRecord(0)->getAccount();
-
+    }
 }
