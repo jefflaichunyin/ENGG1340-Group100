9899b2b387e9082033db52c7ad5d07d4b353d1cf
kiwicheng <vchengym@hku.hk>
2019-04-21 12:19:01 +0800

    add exit function and save user info

--- ui.cpp
+++ ui.cpp
@@ -17,9 +17,18 @@ int uimenu1(void){
     cout << "6. Back to main menu\n";
 }
 
-//void uiprintrecord(){
+/*void uiprintrecord(string username,UserAccounts user_accounts){
+
+       User *user = user_accounts.getUser(username);
+                user->loadRecords(); 
+                 Records *userRecords = user->getUserRecords();
+    for(unsigned int i=0; i<userRecords->countRecord(); i++)
+    {
+        Record *r=userRecords->getRecord(i);
+        cout << r->getDate() << " " << r->getAccount() << " " << r->getCategory() << " " << r->getAmount() << endl;
+    }
     
-//}
+}*/
 
 void startUI()
 {
@@ -27,9 +36,10 @@ void startUI()
     string *username= new string;    //need to delete!!!
     string *userpw= new string;
     UserAccounts user_accounts;
-    cout << "start\n";
-    cout << "1. Log in\n2. Create account"<<endl;
-    while(choice!=1 && choice!= 2){
+    
+    cout << "start\n";  
+    while(choice!=3){
+        cout << "1. Log in\n2. Create account\n3. Exit"<<endl;
         
         cin >> choice;   //get user preference
         if(choice==1){   //log in
@@ -41,12 +51,13 @@ void startUI()
         getline(cin, *userpw);
         if(user_accounts.loadUserInfo()){
             if(user_accounts.checkPassword(*username,*userpw)){ 
-                clrscr();
+    
                 User *user = user_accounts.getUser(*username);
-                user->loadRecords();   
-                //uiprintrecord();
+                //user->loadRecords();   
+                //uiprintrecord(*username,user_accounts);
                 //int recordway = uimenu1();
             }
+            
         }
 
 
@@ -67,6 +78,9 @@ void startUI()
         cout<< user_accounts.addUser(*username,*userpw)<<endl;
         //cout<<string(100,'\n');
 
+        user_accounts.saveUserInfo();
+        User(*username, *userpw);
+        
 
         }
 
